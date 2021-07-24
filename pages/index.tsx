08bbc8b403587826e7d0632887f1111e04e3a91c
redspider926 <stonetiger1225@gmail.com>
2021-07-24 21:42:25 +0800

    theme management

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,17 @@ import Image from 'next/image'
 import React from 'react'
 
 export default function Home() {
-  const [theme, setTheme] = React.useState('dark');
+  const [theme, setTheme] = React.useState('light');
+
+  const colors: any = {
+    light: {
+      color: '#786394',
+    },
+
+    dark: {
+      color: '#ffffff',
+    }
+  }
 
   return (
     <div>
@@ -13,7 +23,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="apppad">
+      <div className="apppad" style={{ backgroundImage: theme === 'light' ? "url('./bg-apppad_light.svg')" : "url('./bg-apppad.svg')" }}>
         <div>
           <div className="apppad-title">
             <div className="apppad-title-container">
@@ -24,11 +34,11 @@ export default function Home() {
 
           <div className="apppad-theme-controller">
             <div className="apppad-theme-controller__switch">
-              <Image src="/sun-active.svg" width={10} height={10} alt="sun" />
-              <div style={{ fontSize: 36 }}>{`/`}</div>
-              <Image src="/moon.svg" width={10} height={10} alt="moon" />
+              <Image src={theme === 'light' ? "/sun-active.svg" : "/sun.svg"} width={10} height={10} alt="sun" onClick={() => setTheme('light')} />
+              <div style={{ fontSize: 36, color: theme === 'light' ? '#786394' : '#fff', fontWeight: 'bolder' }}>{`/`}</div>
+              <Image src={theme === 'dark' ? "/moon-active.svg" : "/moon.svg"} width={10} height={10} alt="moon" onClick={() => setTheme('dark')} />
             </div>
-            <div className="apppad-theme-controller__text">Multichain Defi Suite</div>
+            <div className="apppad-theme-controller__text" style={{ color: colors[theme].color }}>Multichain Defi Suite</div>
           </div>
 
           <div className="apppad-container-border">
@@ -64,7 +74,6 @@ export default function Home() {
               </div>
             </div>
           </div>
-
 
           <div className="apppad-social">
             <div className="apppad-social-icon discord"></div>
